ba934eaaa9ab123d70ec2d8403a76e1a0717d8db
Samsung <Samsung@127.0.0.1>
2015-03-27 19:48:10 +0400

    Added a static method working with memory hashmap

--- src/TextUtilities/MyTextUtilities.java
+++ src/TextUtilities/MyTextUtilities.java
@@ -6,6 +6,7 @@
 package TextUtilities;
 
 import javax.swing.JTextField;
+import java.util.HashMap;
 
 /**
  * Class for some text processing utilities
@@ -37,6 +38,41 @@ public class MyTextUtilities {
             value=Double.valueOf(str);
             return value;
         }
+        return value;
+    }
+    
+    /**
+     * Checks that the entered value is within bounds provided and that the entered value is
+     * actually a double number. In case of error default value from a HashMap is substituted instead.
+     * @param min minimal value
+     * @param max maximal value
+     * @param field text field
+     * @param str default value
+     * @param oldStrings
+     * @return either entered value or default value in case of an error
+     */
+    
+    public static Double TestValueWithMemory(double min, double max, JTextField field,
+            String str, HashMap<JTextField, String> oldStrings) {
+        Double value;
+        String oldStr=oldStrings.get(field);
+        if (oldStr==null) {
+            oldStr=str;
+            oldStrings.put(field, str);
+        }
+        try {
+            value=Double.valueOf(field.getText());
+        } catch (NumberFormatException e) {
+            field.setText(oldStr);
+            value=Double.valueOf(oldStr);
+            return value;
+        }
+        if (value < min || value > max ) {
+            field.setText(oldStr);
+            value=Double.valueOf(oldStr);
+            return value;
+        }
+        oldStrings.replace(field, oldStr);
         return value;
     }
 }

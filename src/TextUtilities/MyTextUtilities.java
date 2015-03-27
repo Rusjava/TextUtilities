955fdff03e8f0120f59787c145636f8908b1e70f
ruslik <ruslik@10.5.1.51>
2015-03-27 15:05:09 +0300

    Created text utilities library

---
+++ src/TextUtilities/MyTextUtilities.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TextUtilities;
+
+import javax.swing.JTextField;
+
+/**
+ * Class for some text processing utilities
+ * @author Ruslan feshchenko
+ * @version 0.1
+ */
+public class MyTextUtilities {
+
+    /**
+     * Checks that the entered value is within bounds provided and that the entered value is
+     * actually a double number. In case of error default value is substituted instead.
+     * @param min minimal value
+     * @param max maximal value
+     * @param field text field
+     * @param str default value
+     * @return either entered value or default value in case of an error
+     */
+    public static Double TestValue(double min, double max, JTextField field, String str) {
+        Double value;
+        try {
+            value=Double.valueOf(field.getText());
+        } catch (NumberFormatException e) {
+            field.setText(str);
+            value=Double.valueOf(str);
+            return value;
+        }
+        if (value < min || value > max ) {
+            field.setText(str);
+            value=Double.valueOf(str);
+            return value;
+        }
+        return value;
+    }
+}

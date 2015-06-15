3775863814609e7a8e8337fd2cf9b401f30ae788
Samsung <Samsung@127.0.0.1>
2015-06-15 09:07:49 +0400

    Added a new method for JavaFX fields. Added code handling zero length strings

--- src/TextUtilities/MyTextUtilities.java
+++ src/TextUtilities/MyTextUtilities.java
@@ -7,6 +7,7 @@ package TextUtilities;
 
 import javax.swing.JTextField;
 import java.util.Map;
+import javafx.scene.control.TextField;
 
 /**
  * Class for some text processing utilities
@@ -26,8 +27,12 @@ public class MyTextUtilities {
      */
     public static Double TestValue(double min, double max, JTextField field, String str) {
         Double value;
+        String newStr = field.getText();
+        if (newStr.length() == 0) {
+            return 0.0;
+        }
         try {
-            value=Double.valueOf(field.getText());
+            value=Double.valueOf(newStr);
         } catch (NumberFormatException e) {
             field.setText(str);
             value=Double.valueOf(str);
@@ -57,6 +62,9 @@ public class MyTextUtilities {
         Double value;
         String oldStr=oldStrings.get(field);
         String newStr=field.getText();
+        if (newStr.length() == 0) {
+            return 0.0;
+        }
         if (oldStr==null) {
             oldStr=str;
             oldStrings.put(field, str);
@@ -73,6 +81,46 @@ public class MyTextUtilities {
             value=Double.valueOf(oldStr);
             return value;
         }
+        oldStrings.replace(field, newStr);
+        return value;
+    }
+    
+     /**
+     * Checks that the entered value is within bounds provided and that the entered value is
+     * actually a double number. In case of error default value from a HashMap is substituted instead.
+     * @param min minimal value
+     * @param max maximal value
+     * @param field text field
+     * @param str default value
+     * @param oldStrings
+     * @return either entered value or default value in case of an error
+     */
+    
+    public static Double TestValueWithMemory(double min, double max, TextField field,
+            String str, Map<TextField, String> oldStrings) {
+        Double value;
+        String oldStr = oldStrings.get(field);
+        String newStr = field.getText();
+        if (newStr.length() == 0) {
+            return 0.0;
+        }
+        if (oldStr == null) {
+            oldStr = str;
+            oldStrings.put(field, str);
+        }
+        try {
+            value = Double.valueOf(newStr);
+        } catch (NumberFormatException e) {
+            field.setText(oldStr);
+            value = Double.valueOf(oldStr);
+            return value;
+        }
+        if (value < min || value > max) {
+            field.setText(oldStr);
+            value = Double.valueOf(oldStr);
+            return value;
+        }
+        
         oldStrings.replace(field, newStr);
         return value;
     }
